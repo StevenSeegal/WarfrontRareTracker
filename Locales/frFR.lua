92fa4fc92b00d92d59ed9edb55f5b6a8f84fa31d
StevenSeegal <stvnseegal@gmail.com>
2018-10-11 11:59:31 +0200

    Locales

---
+++ Locales/frFR.lua
@@ -0,0 +1,40 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("WarfrontRareTracker", "frFR")
+if not L then return end
+
+--------
+-- Rares
+L["dooms_howl"] = "Le Rugissement du Lion"
+L["the_lions_roar"] = "Héraut du malheur"
+L["cresting_goliath"] = "Goliath déferlant"
+L["burning_goliath"] = "Goliath ardent"
+L["rumbling_goliath"] = "Goliath grondant"
+L["thundering_goliath"] = "Goliath fulminant"
+L["beastrider_kama"] = "Chevaucheur de bêtes Kama"
+L["nimar_the_slayer"] = "Nimar le Pourfendeur "
+L["overseer_krix"] = "Surveillant Krix"
+L["skullripper"] = "Arrache-Crâne"
+L["knight_captain_aldrin"] = "Chevalier-capitaine Aldrin"
+L["doomrider_helgrim"] = "Chevauche-malheur Sombrenfer"
+L["branchlord_aldrus"] = "Seigneur-branche Aldrus"
+L["darbel_montrose"] = "Darbel Montrose"
+L["echo_of_myzrael"] = "Echo de Myzrael"
+L["fozruk"] = "Fozruk"
+L["man_hunter_rog"] = "Rog le Chasseur d'hommes"
+L["plaguefeather"] = "Pesteplume"
+L["ragebeak"] = "Ragebec"
+L["venomarus"] = "Venimarus"
+L["yogursa"] = "Yogursa"
+L["foulbelly"] = "Souillebedon"
+L["geomancer_flintdagger"] = "Géomancien Dague-de-silex"
+L["horrific_apparition"] = "Apparition horrifique"
+L["korgresh_coldrage"] = "Kor'gresh Froiderage"
+L["kovork"] = "Kovork"
+L["molok_the_crusher"] = "Molok l'Anéantisseur"
+L["ruul_onestone"] = "Ruul Unepierre"
+L["singer"] = "Singer"
+L["zalas_witherbark"] = "Zalas Fanécorce"
+
+
+-------------------------------------------------------------------------------------
+-- more translations follows after i've implemented some new features I have in mind.
+-- At that point most of the addon is complete so it's best to wait for that.
